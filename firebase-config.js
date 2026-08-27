b707a6f43a46b8d554bb9ca56be1f53121c856ef
keithjmorris <keith.morris@outlook.com>
2026-08-27 13:10:36 +0100

    Correct error in Firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,14 +4,12 @@
 
 export const firebaseConfig = {
   apiKey: "AIzaSyBiArjP7lglfHRFCxVpNdymQeDpUBo01co",
-  authDomain: "carchat-86225.firebaseapp.com",
-  projectId: "carchat-86225",
-  storageBucket: "carchat-86225.firebasestorage.app",
-  messagingSenderId: "977512816928",
-  appId: "1:977512816928:web:6e7a177f2886365f35f745"
+ authDomain: "carchat-86225.firebaseapp.com",
+ projectId: "carchat-86225",
+ storageBucket: "carchat-86225.firebasestorage.app",
+ messagingSenderId: "977512816928",
+ appId: "1:977512816928:web:6e7a177f2886365f35f745"
 };
-
-export const vapidKey = "BHksnOaju9ebQ2RpXizaQaMck3c29I4tOi1uYp-CeVcyR5F5bZBdL3h1MWAY8Upb7IrnXhJ0tm0cO3TqpXeZfgE";
 
 // For web push notifications: Firebase Console > Project Settings >
 // Cloud Messaging > Web Push certificates > Generate key pair.
